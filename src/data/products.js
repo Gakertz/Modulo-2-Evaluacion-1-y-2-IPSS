6d85cfb5a8ed04c57980d96d22012edf2b090b98
Tomas Alvarez Chavez <93352642+Gakertz@users.noreply.github.com>
2026-06-12 17:07:35 -0400

    Docs: creacion de datos de productos segun instrucciones

---
+++ src/data/products.js
@@ -0,0 +1,44 @@
+export const productos = [
+  {
+    id: 1,
+    name: 'Audífonos Bluetooth',
+    price: 24990,
+    category: 'Tecnología',
+    image: 'https://www.todoelectroofertas.cl/wp-content/uploads/2024/12/96-2024-12-30T194318.686.png'
+  },
+  {
+    id: 2,
+    name: 'Mochila dragon ball',
+    price: 18990,
+    category: 'Accesorios',
+    image: 'https://www.tiendacopec.cl/cdn/shop/files/DBS103-BP081A_2.jpg?v=1753410017&width=1200'
+  },
+  {
+    id: 3,
+    name: 'Reloj deportivo',
+    price: 32990,
+    category: 'Deporte',
+    image: 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/070b0e8bf0f6ed77fc082157bd84bbff.jpg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp'
+  },
+  {
+    id: 4,
+    name: 'Zapatillas',
+    price: 45990,
+    category: 'Deporte',
+    image: 'https://www.joma.com.pe/wp-content/uploads/2025/06/zapatillas-deportivas.jpg'
+  },
+  {
+    id: 5,
+    name: 'Cafetera',
+    price: 27990,
+    category: 'Hogar',
+    image: 'https://www.kitchencenter.cl/cdn/shop/files/amb4-web.jpg?v=1710961959&width=1445'
+  },
+  {
+    id: 6,
+    name: 'Lámpara de escritorio',
+    price: 15990,
+    category: 'Hogar',
+    image: 'https://www.ikea.com/cl/es/images/products/naevlinge-lampara-de-escritorio-led-negro__1286171_pe933454_s5.jpg?f=s'
+  }
+];
