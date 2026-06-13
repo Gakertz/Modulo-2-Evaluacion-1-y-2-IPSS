02154a3fb5d56f137415110b24f5326c5be7905b
Tomas Alvarez Chavez <93352642+Gakertz@users.noreply.github.com>
2026-06-13 12:50:09 -0400

    chore: logo agregado segun instrucciones de la tarea

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 function Header() {
   return (
     <header className="encabezado">
+      <img src="https://cdn.creativefabrica.com/2021/09/20/ECommerce-logo-design-Graphics-17588014-1.jpg" alt="Logo ecommerce extraido de google" className="logo" />
       <h1>Ecommmerce Simple</h1>
       <p>Productos para comprar</p>
     </header>
