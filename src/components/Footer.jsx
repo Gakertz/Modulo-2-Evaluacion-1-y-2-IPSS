8845e20938d5b104756a586e89dd702ab41300fc
Tomas Alvarez Chavez <93352642+Gakertz@users.noreply.github.com>
2026-06-27 10:35:00 -0400

    Chore: Actualización básica del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ function Footer() {
   return (
     <footer className="pie-pagina">
       <p>Proyecto de ecommerce simple para evaluacion IPSS</p>
+      <p>Productos obtenidos desde https://dummyjson.com/products</p>
     </footer>
   );
 }
-
 export default Footer;
